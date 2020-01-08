3b8f105b95aea4466654082715b51aac20af06d4
Sam Delay <samuel.delay@gmail.com>
2020-01-08 18:34:25 -0500

    fixed submit function

--- script.js
+++ script.js
@@ -9,36 +9,36 @@ const uppercaseCharCodes = arrayFromLowToHigh(65, 90);
 const lowercaseCharCodes = arrayFromLowToHigh(97, 122);
 const numericCharCodes = arrayFromLowToHigh(48, 57);
 const specialCharCodes = arrayFromLowToHigh(33, 47).concat(
-    arrayFromLowToHigh(58,64)
+    arrayFromLowToHigh(58, 64)
 ).concat(
     arrayFromLowToHigh(91, 96)
 ).concat(
-    arrayFromLowToHigh(123,126)
+    arrayFromLowToHigh(123, 126)
 );
 
+console.log(numOfCharacters)
 
-
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    numOfCharacters = numOfCharacters.value;
-    special = special.checked;
-    numeric = numeric.checked;
-    uppercase = uppercase.checked;
-    password = genereratePassword(numOfCharacters, special, numeric, uppercase);
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    let numOfCharactersval = numOfCharacters.value;
+    let specialval = special.checked;
+    let numericval = numeric.checked;
+    let uppercaseval = uppercase.checked;
+    let password = genereratePassword(numOfCharactersval, specialval, numericval, uppercaseval);
     passwordDisplay.innerText = password;
 })
 
-function genereratePassword(numberOfCharacters, special, numeric, uppercase) {
+function genereratePassword(numOfCharacters, specialval, numericval, uppercaseval) {
     let charCodes = lowercaseCharCodes;
-    if (uppercase) charCodes = charCodes.concat
-    (uppercaseCharCodes);
-    if (numeric) charCodes = charCodes.concat
-    (numericCharCodes);
-    if (special) charCodes = charCodes.concat
-    (specialCharCodes);
+    if (uppercaseval) charCodes = charCodes.concat
+        (uppercaseCharCodes);
+    if (numericval) charCodes = charCodes.concat
+        (numericCharCodes);
+    if (specialval) charCodes = charCodes.concat
+        (specialCharCodes);
 
     const passwordCharacters = []
-    for(let i = 0; i < numOfCharacters; i++) {
+    for (let i = 0; i < numOfCharacters; i++) {
         const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
         passwordCharacters.push(String.fromCharCode(characterCode));
     }
